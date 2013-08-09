ac3e55f5009e839837df783a7c1a32109fdd07d8
snoeg <mikrat@bio4-ett.utu.fi>
2013-08-09 18:50:16 +0300

    Make input pin known to user on serial port

--- Github_Tutorial.ino
+++ Github_Tutorial.ino
@@ -24,7 +24,7 @@ void loop()
   int myValue = 0;
   myValue = analogRead(A0);
   
-  Serial.print("The value is: ");
+  Serial.print("The value on A0 is: ");
   Serial.println(myValue);
 
   delay(250);
