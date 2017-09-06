5c0326a344133302e15b73ebe2c6adfe5cb18b0c
Maurizio Malaspina <iw6dfw@gmail.com>
2017-09-06 14:15:21 +0200

    another commit!
    the same as before before

--- Github_Tutorial.ino
+++ Github_Tutorial.ino
@@ -2,11 +2,11 @@
  12-29-2012
  Spark Fun Electronics
  Nathan Seidle
- 
+
  This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- 
+
  This is bad code but is used to demonstrate how to use repositories on github.
- 
+
  If we have an analog sensor such as a trimpot or flex sensor on A0, this code would attempt
  to read it and output it to the terminal at 9600bps.
 */
@@ -14,19 +14,18 @@
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Trimpot read example modified by Maux");
+  Serial.println("Trimpot read example modified by Maux two times");
 
   pinMode(A0, INPUT);
 }
 
-void loop() 
+void loop()
 {
   int myValue = 0;
-  myValue = analogRead(A0);
-  
+  myValue = analogRead(A0); // add a comment
+
   Serial.print("The value is: ");
   Serial.println(myValue);
 
   delay(250);
 }
-
